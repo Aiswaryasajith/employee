df215f71d30957921c4f069a9fb4e20053e01fc2
Aiswaryasajith <aiswaryasajith99@gmail.com>
2021-03-17 15:43:19 +0530

    change2

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,7 @@
+@extends("theme")
+@section("content")
+<div class="row">
+<div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum ratione voluptate repellendus repellat et sapiente ullam consequuntur voluptatibus, voluptates minima dolorem quia quas sequi labore, sunt dicta perferendis consectetur itaque.</div>
+<div class="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat inventore eligendi dolores vel unde quis doloribus, magnam harum nobis ipsam quo soluta accusamus libero dicta molestiae cupiditate commodi veritatis vitae!</div>
+</div>
+@endsection
